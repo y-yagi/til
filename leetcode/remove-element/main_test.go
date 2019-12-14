07907d280dcc53c33915258a6faff55ff9313f3f
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-12-14 11:02:32 +0900

    Add remove-element

---
+++ leetcode/remove-element/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+var tests = []struct {
+	in1  []int
+	in2  int
+	want int
+}{
+	{[]int{3, 2, 2, 3}, 3, 2},
+	{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2, 5},
+}
+
+func TestRemoveElement(t *testing.T) {
+	for _, tt := range tests {
+		got := removeElement(tt.in1, tt.in2)
+		if got != tt.want {
+			t.Fatalf("in1 %v, in2 %v, got %v, want %v", tt.in1, tt.in2, got, tt.want)
+		}
+	}
+}
