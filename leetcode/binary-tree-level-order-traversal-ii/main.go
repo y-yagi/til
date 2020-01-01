53f6804012bebc8bfe87cd214d23cf74063f1d4a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-01-01 20:05:24 +0900

    Add binary-tree-level-order-traversal-ii

---
+++ leetcode/binary-tree-level-order-traversal-ii/main.go
@@ -0,0 +1,35 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrderBottom(root *TreeNode) [][]int {
+	var answer [][]int
+	if root == nil {
+		return answer
+	}
+
+	q := make([]*TreeNode, 0)
+	q = append(q, root)
+
+	for len(q) > 0 {
+		var tmp []int
+		for _, tree := range q {
+			q = q[1:]
+			tmp = append(tmp, tree.Val)
+
+			if tree.Left != nil {
+				q = append(q, tree.Left)
+			}
+			if tree.Right != nil {
+				q = append(q, tree.Right)
+			}
+		}
+
+		answer = append([][]int{tmp}, answer...)
+	}
+	return answer
+}
