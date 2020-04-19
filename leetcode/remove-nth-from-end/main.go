a6c562ca19b3d4451a235a64db94b90bf79c73de
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-04-19 19:02:32 +0900

    Add remove-nth-from-end

---
+++ leetcode/remove-nth-from-end/main.go
@@ -0,0 +1,24 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{Val: 0}
+	dummy.Next = head
+	first := dummy
+	second := dummy
+
+	for i := 1; i <= n+1; i++ {
+		first = first.Next
+	}
+
+	for first != nil {
+		first = first.Next
+		second = second.Next
+	}
+	second.Next = second.Next.Next
+	return dummy.Next
+}
---
+++ leetcode/remove-nth-from-end/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+var tests = []struct {
+	in1  *ListNode
+	in2  int
+	want *ListNode
+}{
+	{buildList([]int{1, 2, 3, 4, 5}), 2, buildList([]int{1, 2, 3, 5})},
+}
+
+func TestIsValid(t *testing.T) {
+	for _, tt := range tests {
+		got := removeNthFromEnd(tt.in1, tt.in2)
+		if diff := cmp.Diff(tt.want, got); diff != "" {
+			t.Fatalf("mismatch (-want +got):\n%s", diff)
+		}
+	}
+}
+
+func (x *ListNode) Equal(y *ListNode) bool {
+	if x == nil || y == nil {
+		return false
+	}
+	return x.Val == y.Val
+}
+
+func buildList(s []int) *ListNode {
+	var result *ListNode
+	var before *ListNode
+
+	for _, v := range s {
+		result = &ListNode{Val: v}
+		if before != nil {
+			result.Next = before
+		}
+		before = result
+	}
+
+	return result
+}
