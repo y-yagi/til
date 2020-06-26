690bdf8c807e7d9caa0d221fee504815a25e5642
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-06-26 12:45:05 +0900

    Add Construct Binary Tree from Inorder and Postorder Traversal

---
+++ leetcode/construct-binary-tree-from-inorder-and-postorder-traversal/main.go
@@ -0,0 +1,29 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTreeHelper(post []int, postStart int, postEnd int, inStart int, inPos map[int]int) *TreeNode {
+	if postStart > postEnd {
+		return nil
+	}
+
+	root := &TreeNode{Val: post[postEnd]}
+	rootIdx := inPos[post[postEnd]]
+	leftLen := rootIdx - inStart
+	root.Left = buildTreeHelper(post, postStart, postStart+leftLen-1, inStart, inPos)
+	root.Right = buildTreeHelper(post, postStart+leftLen, postEnd-1, rootIdx+1, inPos)
+
+	return root
+}
+
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	inPos := make(map[int]int)
+	for i := 0; i < len(inorder); i++ {
+		inPos[inorder[i]] = i
+	}
+	return buildTreeHelper(postorder, 0, len(postorder)-1, 0, inPos)
+}
