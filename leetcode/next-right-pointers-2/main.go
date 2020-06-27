33dbb24049a285ef2477b600914cc636eec25d0f
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-06-27 12:38:04 +0900

    Add Populating Next Right Pointers in Each Node II

---
+++ leetcode/next-right-pointers-2/main.go
@@ -0,0 +1,42 @@
+package main
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+
+	queue := make([]*Node, 0)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		qSize := len(queue)
+
+		for i := 0; i < qSize; i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			if i < qSize-1 {
+				node.Next = queue[0]
+			} else {
+				node.Next = nil
+			}
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+
+	return root
+}
