4a4b07c8f4db0d470bdd4cbf3bdbc373cd149898
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-07-06 07:52:51 +0900

    Update

--- leetcode/template/stack-and-dfs.go
+++ leetcode/template/stack-and-dfs.go
@@ -5,6 +5,7 @@ type Node struct {
 	Neighbors []*Node
 }
 
+// With recursion.
 func dfs(cur, target *Node, visited map[*Node]*Node) bool {
 	if cur == target {
 		return true
@@ -15,6 +16,30 @@ func dfs(cur, target *Node, visited map[*Node]*Node) bool {
 			visited[next] = next
 			if dfs(next, target, visited) {
 				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// Without recursion(for avoid stack overflow).
+func dfs2(root, target *Node) bool {
+	visited := map[*Node]*Node{}
+	stack := []*Node{root}
+
+	for len(stack) != 0 {
+		cur := stack[0]
+		stack = stack[1:]
+
+		if cur == target {
+			return true
+		}
+
+		for _, next := range cur.Neighbors {
+			if _, found := visited[next]; !found {
+				visited[next] = next
+				stack = append(stack, next)
 			}
 		}
 	}
