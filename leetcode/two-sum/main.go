fd1f1d91bfc4566c91318d1b4a6caa7260e66753
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-07-11 13:29:17 +0900

    Update

--- leetcode/two-sum/main.go
+++ leetcode/two-sum/main.go
@@ -1,31 +1,17 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Printf("%v\n", twoSum([]int{2, 7, 11, 15}, 9))
-	fmt.Printf("%v\n", twoSum([]int{15, 1, 11, 0, 8}, 9))
-}
-
 func twoSum(nums []int, target int) []int {
-	var fvalue, findex int
-	var answer []int
+	dict := map[int]int{}
 
-	for {
-		if len(nums) == 0 {
-			break
-		}
-
-		fvalue, nums = nums[0], nums[1:]
-
-		for i, num := range nums {
-			if fvalue+num == target {
-				answer = append(answer, findex, i+findex+1)
-				break
-			}
-		}
-		findex++
+	for i := 0; i < len(nums); i++ {
+		dict[target-nums[i]] = i
 	}
 
-	return answer
+	for i := 0; i < len(nums); i++ {
+		if v, found := dict[nums[i]]; found && v != i {
+			return []int{i, v}
+		}
+	}
+
+	return []int{}
 }
