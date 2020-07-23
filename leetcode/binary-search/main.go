232e8fedaeff7f2316ba7cf3a178f6b9e3307e2a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-07-23 14:58:57 +0900

    Add Binary Search

---
+++ leetcode/binary-search/main.go
@@ -0,0 +1,32 @@
+package main
+
+func search(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	if len(nums) == 1 {
+		if nums[0] == target {
+			return 0
+		}
+		return -1
+	}
+
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
---
+++ leetcode/binary-search/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var tests = []struct {
+	in1  []int
+	in2  int
+	want int
+}{
+	{[]int{5}, 5, 0},
+	{[]int{-1, 0, 3, 5, 9, 12}, 9, 4},
+	{[]int{-1, 0, 3, 5, 9, 12}, 2, -1},
+}
+
+func TestIsValid(t *testing.T) {
+	for _, tt := range tests {
+		got := search(tt.in1, tt.in2)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Fatalf("in1 %v, got %v, want %v", tt.in1, got, tt.want)
+		}
+	}
+}
