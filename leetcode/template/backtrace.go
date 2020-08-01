2d33e1523d21550b97c48a1ba1a062e57c16d011
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-08-01 12:38:49 +0900

    Update

--- leetcode/template/backtrace.go
+++ leetcode/template/backtrace.go
@@ -16,3 +16,25 @@ func bactrace(candidate int) {
 		}
 	}
 }
+
+// Real example
+func combine(n int, k int) [][]int {
+	ans := [][]int{}
+	curr := []int{}
+	backtrace(n, k, 1, curr, &ans)
+	return ans
+}
+
+func backtrace(n, k, first int, curr []int, ans *[][]int) {
+	if len(curr) == k {
+		b := make([]int, len(curr))
+		copy(b, curr)
+		*ans = append(*ans, b)
+	}
+
+	for i := first; i < n+1; i++ {
+		curr = append(curr, i)
+		backtrace(n, k, i+1, curr, ans)
+		curr = curr[:len(curr)-1]
+	}
+}
