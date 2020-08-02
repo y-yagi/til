45a3006a138ee1a4653c2f6bb668638d659a0599
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-08-02 14:02:59 +0900

    Update

--- leetcode/same-tree/main.go
+++ leetcode/same-tree/main.go
@@ -7,50 +7,17 @@ type TreeNode struct {
 }
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	if p == nil {
-		if q != nil {
-			return false
-		}
+	if p == nil && q == nil {
 		return true
-	} else if q == nil {
-		return false
-	} else if p.Val != q.Val {
+	}
+
+	if p == nil || q == nil {
 		return false
 	}
 
-	if p.Right != nil {
-		if q.Right == nil {
-			return false
-		} else {
-			if p.Right.Val != q.Right.Val {
-				return false
-			} else {
-				answer := isSameTree(p.Right, q.Right)
-				if answer == false {
-					return false
-				}
-			}
-		}
-	} else if q.Right != nil {
+	if p.Val != q.Val {
 		return false
 	}
 
-	if p.Left != nil {
-		if q.Left == nil {
-			return false
-		} else {
-			if p.Left.Val != q.Left.Val {
-				return false
-			} else {
-				answer := isSameTree(p.Left, q.Left)
-				if answer == false {
-					return false
-				}
-			}
-		}
-	} else if q.Left != nil {
-		return false
-	}
-
-	return true
+	return isSameTree(p.Right, q.Right) && isSameTree(p.Left, q.Left)
 }
