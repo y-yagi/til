8c1b22857920ffc5f208017dd364f434ae6c26d3
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-08-08 14:52:25 +0900

    Update

--- leetcode/remove-element/main.go
+++ leetcode/remove-element/main.go
@@ -1,12 +1,13 @@
 package main
 
 func removeElement(nums []int, val int) int {
+	j := 0
 	for i := 0; i < len(nums); i++ {
-		if nums[i] == val {
-			nums = append(nums[:i], nums[i+1:]...)
-			i--
+		if nums[i] != val {
+			nums[i] = nums[j]
+			j++
 		}
 	}
 
-	return len(nums)
+	return j
 }
