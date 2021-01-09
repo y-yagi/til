938d49f63add869744b5366d9a38ae1894f6c022
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-01-09 11:25:14 +0900

    Use dict version

--- leetcode/pairs-of-songs-with-total-durations-divisible-by-60/main.go
+++ leetcode/pairs-of-songs-with-total-durations-divisible-by-60/main.go
@@ -2,14 +2,12 @@ package main
 
 func numPairsDivisibleBy60(time []int) int {
 	ans := 0
-	remainders := [60]int{}
+	count := map[int]int{}
 	for _, t := range time {
-		if t%60 == 0 { // check if a%60==0 && b%60==0
-			ans += remainders[0]
-		} else { // check if a % 60+b%60==60
-			ans += remainders[60-t%60]
-		}
-		remainders[t%60]++
+		// (time[i] + time[j]) % 60 == (60 - t % 60) % 60
+		ans += count[(60-t%60)%60]
+		count[t%60]++
 	}
+
 	return ans
 }
