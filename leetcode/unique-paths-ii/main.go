e31f4c115b3d2fde92c2453033b00636707c741f
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-01-31 11:26:43 +0900

    Update

--- leetcode/unique-paths-ii/main.go
+++ leetcode/unique-paths-ii/main.go
@@ -4,37 +4,32 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 	m := len(obstacleGrid)
 	n := len(obstacleGrid[0])
 
-	if obstacleGrid[0][0] == 1 {
-		return 0
+	dp := [][]int{}
+	for i := 0; i < m; i++ {
+		dp = append(dp, make([]int, n))
 	}
 
-	obstacleGrid[0][0] = 1
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if obstacleGrid[i][j] == 1 {
+				dp[i][j] = 0
+				continue
+			}
 
-	for i := 1; i < m; i++ {
-		if obstacleGrid[i][0] == 0 && obstacleGrid[i-1][0] == 1 {
-			obstacleGrid[i][0] = 1
-		} else {
-			obstacleGrid[i][0] = 0
-		}
-	}
+			if i == 0 && j == 0 {
+				dp[i][j] = 1
+				continue
+			}
 
-	for i := 1; i < n; i++ {
-		if obstacleGrid[0][i] == 0 && obstacleGrid[0][i-1] == 1 {
-			obstacleGrid[0][i] = 1
-		} else {
-			obstacleGrid[0][i] = 0
-		}
-	}
-
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			if obstacleGrid[i][j] == 0 {
-				obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+			if i == 0 {
+				dp[i][j] = dp[i][j-1]
+			} else if j == 0 {
+				dp[i][j] = dp[i-1][j]
 			} else {
-				obstacleGrid[i][j] = 0
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
 			}
 		}
 	}
 
-	return obstacleGrid[m-1][n-1]
+	return dp[m-1][n-1]
 }
