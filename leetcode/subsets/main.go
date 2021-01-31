dd66f049b2f3f21c804af316084de3175e2f6681
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-01-31 11:51:52 +0900

    Update

--- leetcode/subsets/main.go
+++ leetcode/subsets/main.go
@@ -1,46 +1,19 @@
 package main
 
-import (
-	"math"
-	"strconv"
-)
-
 func subsets(nums []int) [][]int {
-	answer := [][]int{}
-	n := float64(len(nums))
-	for i := int(math.Pow(2, n)); i < int(math.Pow(2, n+1)); i++ {
-		bitmask := strconv.FormatInt(int64(i), 2)
-		bitmask = bitmask[1:]
-
-		curr := []int{}
-		for j := 0; j < int(n); j++ {
-			if bitmask[j] == '1' {
-				curr = append(curr, nums[j])
-			}
-		}
-		answer = append(answer, curr)
-	}
-
-	return answer
+	ans := [][]int{}
+	backtrace(nums, 0, []int{}, &ans)
+	return ans
 }
 
-func subsets_Recursion(nums []int) [][]int {
-	answer := [][]int{}
-	answer = append(answer, []int{})
+func backtrace(nums []int, index int, curr []int, ans *[][]int) {
+	dup := make([]int, len(curr))
+	copy(dup, curr)
+	*ans = append(*ans, dup)
 
-	for _, n := range nums {
-		newSubsets := [][]int{}
-		for _, curr := range answer {
-			a := make([]int, len(curr))
-			copy(a, curr)
-			a = append(a, n)
-			newSubsets = append(newSubsets, a)
-		}
-
-		for _, curr := range newSubsets {
-			answer = append(answer, curr)
-		}
+	for ; index < len(nums); index++ {
+		curr = append(curr, nums[index])
+		backtrace(nums, index+1, curr, ans)
+		curr = curr[:len(curr)-1]
 	}
-
-	return answer
 }
