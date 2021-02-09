cea059a7169cff5066fb5fe168daee0e96092495
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-02-10 08:15:42 +0900

    Update

--- leetcode/number-of-islands/main.go
+++ leetcode/number-of-islands/main.go
@@ -4,26 +4,28 @@ func numIslands(grid [][]byte) int {
 	if len(grid) == 0 {
 		return 0
 	}
+
 	islands := 0
 	for i := range grid {
 		for j := range grid[i] {
-			islands += walk(i, j, grid)
+			islands += dfs(i, j, grid)
 		}
 	}
 
 	return islands
 }
 
-func walk(i, j int, grid [][]byte) int {
-	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[0]) || grid[i][j] == '2' {
+func dfs(i, j int, grid [][]byte) int {
+	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[0]) || grid[i][j] == '0' {
 		return 0
 	}
+
 	if grid[i][j] == '1' {
-		grid[i][j] = '2'
-		walk(i-1, j, grid)
-		walk(i+1, j, grid)
-		walk(i, j-1, grid)
-		walk(i, j+1, grid)
+		grid[i][j] = '0'
+		dfs(i-1, j, grid)
+		dfs(i+1, j, grid)
+		dfs(i, j-1, grid)
+		dfs(i, j+1, grid)
 		return 1
 	}
 	return 0
