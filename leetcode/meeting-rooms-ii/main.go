5aa70b461cc928c967495c34a3c79d3561a8aa11
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-02-13 11:52:51 +0900

    Update(sort version)

--- leetcode/meeting-rooms-ii/main.go
+++ leetcode/meeting-rooms-ii/main.go
@@ -1,28 +1,8 @@
 package main
 
 import (
-	"container/heap"
 	"sort"
 )
-
-type Allocator []int
-
-func (h Allocator) Len() int           { return len(h) }
-func (h Allocator) Less(i, j int) bool { return h[i] < h[j] }
-func (h Allocator) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h Allocator) Peek() int          { return h[0] }
-
-func (h *Allocator) Push(x interface{}) {
-	*h = append(*h, x.(int))
-}
-
-func (h *Allocator) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
 
 func minMeetingRooms(intervals [][]int) int {
 	if len(intervals) == 0 {
@@ -30,16 +10,15 @@ func minMeetingRooms(intervals [][]int) int {
 	}
 
 	sort.Slice(intervals, func(i, j int) bool { return intervals[i][0] < intervals[j][0] })
-	allocator := &Allocator{}
-	heap.Init(allocator)
-	heap.Push(allocator, intervals[0][1])
+	queue := []int{intervals[0][1]}
 
 	for i := 1; i < len(intervals); i++ {
-		if intervals[i][0] >= allocator.Peek() {
-			heap.Pop(allocator)
+		if intervals[i][0] >= queue[0] {
+			queue = queue[1:]
 		}
-		heap.Push(allocator, intervals[i][1])
+		queue = append(queue, intervals[i][1])
+		sort.Ints(queue)
 	}
 
-	return allocator.Len()
+	return len(queue)
 }
