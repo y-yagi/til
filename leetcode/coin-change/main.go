149b9d69929f57ef0e22caff03d39feebe33d93a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-02-14 13:31:55 +0900

    Update

--- leetcode/coin-change/main.go
+++ leetcode/coin-change/main.go
@@ -1,55 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-func d(a ...interface{}) {
-	format := ""
-	for _, _ = range a {
-		format += "%v, "
-	}
-	format += "\n"
-	fmt.Printf(format, a...)
-}
-
 func coinChange(coins []int, amount int) int {
-	if amount < 1 {
-		return 0
+	max := amount + 1
+	dp := make([]int, max)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = max
 	}
 
-	count := make([]int, amount)
-	return dig(coins, amount, count)
-}
-
-func dig(coins []int, rem int, count []int) int {
-	if rem < 0 {
-		return -1
-	}
-
-	if rem == 0 {
-		return 0
-	}
-
-	if count[rem-1] != 0 {
-		return count[rem-1]
-	}
-
-	min := math.MaxInt64
-
-	for _, coin := range coins {
-		res := dig(coins, rem-coin, count)
-		if res >= 0 && res < min {
-			min = 1 + res
+	// DP - Bottom up
+	dp[0] = 0
+	for i := 1; i <= amount; i++ {
+		for j := 0; j < len(coins); j++ {
+			if coins[j] <= i {
+				dp[i] = min(dp[i], dp[i-coins[j]]+1)
+			}
 		}
 	}
 
-	if min == math.MaxInt64 {
-		count[rem-1] = -1
-	} else {
-		count[rem-1] = min
+	if dp[amount] > amount {
+		return -1
 	}
 
-	return count[rem-1]
+	return dp[amount]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
 }
