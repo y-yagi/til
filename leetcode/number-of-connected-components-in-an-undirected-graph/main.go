3cca07b785eb38a93fde79cc7e1b4b7500cf98d9
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-02-15 08:20:09 +0900

    Update

--- leetcode/number-of-connected-components-in-an-undirected-graph/main.go
+++ leetcode/number-of-connected-components-in-an-undirected-graph/main.go
@@ -1,6 +1,7 @@
 package main
 
 func countComponents(n int, edges [][]int) int {
+	// Build Disjoint Set
 	roots := make([]int, n)
 	for i := 0; i < n; i++ {
 		roots[i] = i
@@ -10,6 +11,7 @@ func countComponents(n int, edges [][]int) int {
 		root1 := find(roots, e[0])
 		root2 := find(roots, e[1])
 		if root1 != root2 {
+			// Union two edges
 			roots[root1] = root2
 			n--
 		}
@@ -17,9 +19,10 @@ func countComponents(n int, edges [][]int) int {
 	return n
 }
 
+// Find root from Disjoint Set
 func find(roots []int, id int) int {
 	for roots[id] != id {
-		roots[id] = roots[roots[id]]
+		// roots[id] = roots[roots[id]] // Path compression
 		id = roots[id]
 	}
 	return id
