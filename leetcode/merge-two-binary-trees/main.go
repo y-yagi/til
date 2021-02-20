54e010d41737f8abf40905bf9c0f14380419a187
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-02-20 11:19:46 +0900

    Update

--- leetcode/merge-two-binary-trees/main.go
+++ leetcode/merge-two-binary-trees/main.go
@@ -10,31 +10,12 @@ func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
 	if t1 == nil {
 		return t2
 	}
-
-	stack := [][]*TreeNode{}
-	stack = append(stack, []*TreeNode{t1, t2})
-
-	for len(stack) > 0 {
-		t := stack[0]
-		stack = stack[1:]
-
-		if t[0] == nil || t[1] == nil {
-			continue
-		}
-
-		t[0].Val += t[1].Val
-		if t[0].Left == nil {
-			t[0].Left = t[1].Left
-		} else {
-			stack = append(stack, []*TreeNode{t[0].Left, t[1].Left})
-		}
-
-		if t[0].Right == nil {
-			t[0].Right = t[1].Right
-		} else {
-			stack = append(stack, []*TreeNode{t[0].Right, t[1].Right})
-		}
+	if t2 == nil {
+		return t1
 	}
 
+	t1.Val += t2.Val
+	t1.Left = mergeTrees(t1.Left, t2.Left)
+	t1.Right = mergeTrees(t1.Right, t2.Right)
 	return t1
 }
