08802524f6eb24e2662224f696a0b7edc8d041e3
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-02-20 11:42:06 +0900

    Update

--- leetcode/max-area-of-island/main.go
+++ leetcode/max-area-of-island/main.go
@@ -1,40 +1,29 @@
 package main
 
-import "fmt"
-
-func d(a ...interface{}) {
-	format := ""
-	for _, _ = range a {
-		format += "%v, "
-	}
-	format += "\n"
-	fmt.Printf(format, a...)
-}
-
 func maxAreaOfIsland(grid [][]int) int {
-	seen := [][]bool{}
-	max := 0
-	for i := 0; i < len(grid); i++ {
-		seen = append(seen, make([]bool, len(grid[0])))
-	}
-
+	ans := 0
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[0]); j++ {
-			v := area(i, j, grid, seen)
-			if v > max {
-				max = v
-			}
+			ans = max(ans, helper(i, j, grid))
 		}
 	}
 
-	return max
+	return ans
 }
 
-func area(i, j int, grid [][]int, seen [][]bool) int {
-	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) || seen[i][j] || grid[i][j] == 0 {
+func helper(x, y int, grid [][]int) int {
+	if x < 0 || y < 0 || x > len(grid)-1 || y > len(grid[0])-1 || grid[x][y] == 0 {
 		return 0
 	}
 
-	seen[i][j] = true
-	return 1 + area(i+1, j, grid, seen) + area(i-1, j, grid, seen) + area(i, j+1, grid, seen) + area(i, j-1, grid, seen)
+	grid[x][y] = 0
+	return 1 + helper(x+1, y, grid) + helper(x, y+1, grid) + helper(x-1, y, grid) + helper(x, y-1, grid)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
 }
