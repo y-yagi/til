a8ef2748c5baeca5605cfab345cdc015524ae9d7
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-02-21 11:32:14 +0900

    Update

--- leetcode/kth-largest-element-in-a-stream/main.go
+++ leetcode/kth-largest-element-in-a-stream/main.go
@@ -1,49 +1,52 @@
 package main
 
-import (
-	"sort"
-)
+import "container/heap"
+
+// Ref: https://golang.org/pkg/container/heap/
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
 
 type KthLargest struct {
-	s []int
-	k int
+	nums *IntHeap
+	k    int
 }
 
 func Constructor(k int, nums []int) KthLargest {
-	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
-	l := len(nums)
-	if k < l {
-		l = k
+	h := &IntHeap{}
+	heap.Init(h)
+
+	for i := 0; i < len(nums); i++ {
+		heap.Push(h, nums[i])
 	}
-	obj := KthLargest{s: nums[:l], k: k}
-	return obj
+
+	for len(*h) > k {
+		heap.Pop(h)
+	}
+	return KthLargest{h, k}
 }
 
 func (this *KthLargest) Add(val int) int {
-	if len(this.s) == 0 {
-		this.s = append(this.s, val)
-		return this.s[this.k-1]
+	if len(*this.nums) < this.k {
+		heap.Push(this.nums, val)
+	} else if val > (*this.nums)[0] {
+		heap.Push(this.nums, val)
+		heap.Pop(this.nums)
 	}
-
-	if len(this.s) >= this.k && this.s[this.k-1] >= val {
-		return this.s[this.k-1]
-	}
-
-	if this.s[0] <= val {
-		this.s = append([]int{val}, this.s[:this.k-1]...)
-		return this.s[this.k-1]
-	}
-
-	i := len(this.s) / 2
-	if this.s[i] > val {
-		i = len(this.s) - 1
-	}
-
-	for ; i >= 0; i-- {
-		if this.s[i] > val {
-			this.s = append(this.s[:i+1], append([]int{val}, this.s[i+1:this.k-1]...)...)
-			break
-		}
-	}
-	return this.s[this.k-1]
+	return (*this.nums)[0]
 }
