5614cb3f8a522d510af670cb56887ddf95690d97
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-08-01 12:37:46 +0900

    Add google calendar example

---
+++ go/googlecalendar/go.mod
@@ -0,0 +1,8 @@
+module github.com/y-yagi/til/go/googlecalendar
+
+go 1.16
+
+require (
+	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914 // indirect
+	google.golang.org/api v0.52.0 // indirect
+)
---
+++ go/googlecalendar/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+)
+
+func getClient(config *oauth2.Config) *http.Client {
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func main() {
+	ctx := context.Background()
+	credentials := filepath.Join(os.Getenv("HOME"), ".credentials.json")
+	b, err := ioutil.ReadFile(credentials)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Calendar client: %v", err)
+	}
+
+	calendars, err := srv.CalendarList.List().ShowDeleted(false).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve Calendar list: %v", err)
+	}
+
+	fmt.Println("Upcoming events:")
+	for _, cal := range calendars.Items {
+		t := time.Now().Format(time.RFC3339)
+		events, err := srv.Events.List(cal.Id).ShowDeleted(false).
+			SingleEvents(true).TimeMin(t).OrderBy("startTime").Do()
+		if err != nil {
+			fmt.Printf("Unable to retrieve user's events: %v\n", err)
+		}
+
+		if len(events.Items) != 0 {
+			for _, event := range events.Items {
+				date := event.Start.DateTime
+				if date == "" {
+					date = event.Start.Date
+				}
+				fmt.Printf("%v (%v)\n", event.Summary, date)
+			}
+		}
+
+		for _, reminder := range cal.DefaultReminders {
+			fmt.Printf("%+v\n", reminder)
+		}
+	}
+}
