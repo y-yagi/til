17d6acf2705bae87676309b4071a33ef4bbb25f0
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-03-13 08:48:20 +0900

    Add Valid Parentheses(Rust)

---
+++ leetcode/valid-parentheses/rust/src/lib.rs
@@ -0,0 +1,26 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_valid_test() {
+        assert_eq!(is_valid(String::from("()")), true);
+        assert_eq!(is_valid(String::from("()[]{}")), true);
+        assert_eq!(is_valid(String::from("(]")), false);
+    }
+}
+
+pub fn is_valid(s: String) -> bool {
+    let mut stack = Vec::new();
+    for i in s.chars() {
+        match i {
+            '{' => stack.push('}'),
+            '(' => stack.push(')'),
+            '[' => stack.push(']'),
+            '}'|')'|']' if Some(i) != stack.pop() => return false,
+            _ => (),
+        }
+    }
+
+    return stack.is_empty();
+}
