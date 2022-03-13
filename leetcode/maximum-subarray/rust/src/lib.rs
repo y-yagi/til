9202733973eddf5ccfd8fa1917dd872ddeb260f7
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-03-14 08:10:33 +0900

    Add Maximum Subarray(Rust)

---
+++ leetcode/maximum-subarray/rust/src/lib.rs
@@ -0,0 +1,27 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_valid_test() {
+        assert_eq!(max_sub_array(vec![-2, 1]), 1);
+        assert_eq!(max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5 ,4]), 6);
+        assert_eq!(max_sub_array(vec![1]), 1);
+        assert_eq!(max_sub_array(vec![5, 4, -1, 7, 8]), 23);
+    }
+}
+
+pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+    let (mut cur, mut max) = (nums[0], nums[0]);
+
+
+    for x in 1..nums.len() {
+        cur = cur + nums[x];
+        if nums[x] > cur {
+            cur = nums[x]
+        }
+        max = max.max(cur)
+    }
+
+    max
+}
