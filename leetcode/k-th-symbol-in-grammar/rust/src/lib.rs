ac1add589c9a08090d3727f85a93f01bb2a27f1a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-05-02 07:40:09 +0900

    Add K-th Symbol in Grammar(Rust)

---
+++ leetcode/k-th-symbol-in-grammar/rust/src/lib.rs
@@ -0,0 +1,34 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(Solution::kth_grammar(1, 1), 0);
+        assert_eq!(Solution::kth_grammar(2, 1), 0);
+    }
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn kth_grammar(n: i32, k: i32) -> i32 {
+        if n == 1 { return 0 }
+
+        if k%2 == 0 {
+            if Self::kth_grammar(n-1, k/2) == 0 {
+                return 1
+            } else {
+                return 0
+            }
+        } else {
+            if Self::kth_grammar(n-1, (k+1)/2) == 0 {
+                return 0
+            } else {
+                return 1
+            }
+        }
+
+        0
+    }
+}
