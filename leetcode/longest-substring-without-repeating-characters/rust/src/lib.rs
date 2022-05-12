814f89b90af3fac078f2b023eaa7a5db0ccd5029
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-05-12 09:46:18 +0900

    Update format

--- leetcode/longest-substring-without-repeating-characters/rust/src/lib.rs
+++ leetcode/longest-substring-without-repeating-characters/rust/src/lib.rs
@@ -4,28 +4,32 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(length_of_longest_substring(String::from("abcabcbb")), 3);
-        assert_eq!(length_of_longest_substring(String::from("bbbbb")), 1);
-        assert_eq!(length_of_longest_substring(String::from("pwwkew")), 3);
+        assert_eq!(Solution::length_of_longest_substring(String::from("abcabcbb")), 3);
+        assert_eq!(Solution::length_of_longest_substring(String::from("bbbbb")), 1);
+        assert_eq!(Solution::length_of_longest_substring(String::from("pwwkew")), 3);
     }
 }
+
+struct Solution {}
 
 use std::collections::HashMap;
 use std::cmp::max;
 
-pub fn length_of_longest_substring(s: String) -> i32 {
-    let mut m : HashMap<char, i32> = HashMap::new();
-    let mut ans = 0;
-    let mut before = -1;
-    let mut current = 0;
+impl Solution {
+  pub fn length_of_longest_substring(s: String) -> i32 {
+      let mut m : HashMap<char, i32> = HashMap::new();
+      let mut ans = 0;
+      let mut before = -1;
+      let mut current = 0;
 
-    for c in s.chars() {
-        if let Some(last) = m.insert(c, current) {
-            before = max(before, last);
-        }
-        ans = max(ans, current - before) ;
-        current += 1;
-    }
+      for c in s.chars() {
+          if let Some(last) = m.insert(c, current) {
+              before = max(before, last);
+          }
+          ans = max(ans, current - before) ;
+          current += 1;
+      }
 
-    ans
+      ans
+  }
 }
