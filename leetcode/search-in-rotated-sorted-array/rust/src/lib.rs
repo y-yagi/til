f39fbf1844085b4c27cf179684b2e6ddd5082dc5
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-05-21 07:47:50 +0900

    Format

--- leetcode/search-in-rotated-sorted-array/rust/src/lib.rs
+++ leetcode/search-in-rotated-sorted-array/rust/src/lib.rs
@@ -4,36 +4,40 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(search(vec![4, 5, 6, 7, 0, 1, 2], 0), 4);
-        assert_eq!(search(vec![4, 5, 6, 7, 0, 1, 2], 3), -1);
-        assert_eq!(search(vec![1], 0), -1);
+        assert_eq!(Solution::search(vec![4, 5, 6, 7, 0, 1, 2], 0), 4);
+        assert_eq!(Solution::search(vec![4, 5, 6, 7, 0, 1, 2], 3), -1);
+        assert_eq!(Solution::search(vec![1], 0), -1);
     }
 }
 
-pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-    let mut start: usize = 0;
-    let mut end: usize = nums.len() - 1;
+struct Solution {}
 
-    while start <= end {
-        let mid = start + (end-start)/2;
-        if nums[mid] == target {
-            return mid as i32
-        }
+impl Solution {
+  pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+      let mut start: usize = 0;
+      let mut end: usize = nums.len() - 1;
 
-        if nums[mid] >= nums[start] {
-            if target >= nums[start] && target < nums[mid] {
-                end = mid - 1
-            } else {
-                start = mid + 1
-            }
-        } else {
-            if target <= nums[end] && target > nums[mid] {
-                start = mid + 1
-            } else {
-                end = mid - 1
-            }
-        }
-    }
+      while start <= end {
+          let mid = start + (end-start)/2;
+          if nums[mid] == target {
+              return mid as i32
+          }
 
-    -1
+          if nums[mid] >= nums[start] {
+              if target >= nums[start] && target < nums[mid] {
+                  end = mid - 1
+              } else {
+                  start = mid + 1
+              }
+          } else {
+              if target <= nums[end] && target > nums[mid] {
+                  start = mid + 1
+              } else {
+                  end = mid - 1
+              }
+          }
+      }
+
+      -1
+  }
 }
