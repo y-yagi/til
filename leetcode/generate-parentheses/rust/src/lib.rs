a13da32784266a750f706e51ed4f1f26af0b6ef1
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-05-31 08:05:39 +0900

    Format

--- leetcode/generate-parentheses/rust/src/lib.rs
+++ leetcode/generate-parentheses/rust/src/lib.rs
@@ -4,26 +4,39 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(generate_parenthesis(3), vec!["((()))".to_string(), "(()())".to_string(),  "(())()".to_string(),  "()(())".to_string(), "()()()".to_string()]);
-        assert_eq!(generate_parenthesis(1), vec!["()".to_string()]);
+        assert_eq!(
+            Solution::generate_parenthesis(3),
+            vec![
+                "((()))".to_string(),
+                "(()())".to_string(),
+                "(())()".to_string(),
+                "()(())".to_string(),
+                "()()()".to_string()
+            ]
+        );
+        assert_eq!(Solution::generate_parenthesis(1), vec!["()".to_string()]);
     }
 }
 
-pub fn generate_parenthesis(n: i32) -> Vec<String> {
-    fn backtrace(s: String, open: i32, close: i32) -> Vec<String> {
-        let mut res = vec![];
-        if open == 0 && close == 0 {
-            return vec![s];
+struct Solution {}
+
+impl Solution {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        fn backtrace(s: String, open: i32, close: i32) -> Vec<String> {
+            let mut res = vec![];
+            if open == 0 && close == 0 {
+                return vec![s];
+            }
+
+            if open > 0 {
+                res.append(&mut backtrace(s.clone() + "(", open - 1, close + 1));
+            }
+            if close > 0 {
+                res.append(&mut backtrace(s.clone() + ")", open, close - 1));
+            }
+            res
         }
 
-        if open > 0 {
-            res.append(&mut backtrace(s.clone() + "(", open-1, close+1));
-        }
-        if close > 0 {
-            res.append(&mut backtrace(s.clone() + ")", open, close-1));
-        }
-        res
+        backtrace("".to_string(), n, 0)
     }
-
-    backtrace("".to_string(), n, 0)
 }
