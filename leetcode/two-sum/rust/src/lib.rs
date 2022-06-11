737485c9f00bfe75c115025f0967bcc40074dea9
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-06-12 08:07:17 +0900

    Format

--- leetcode/two-sum/rust/src/lib.rs
+++ leetcode/two-sum/rust/src/lib.rs
@@ -4,20 +4,24 @@ mod tests {
 
     #[test]
     fn two_sum_test1() {
-        assert_eq!(two_sum(vec![2, 7, 11, 15], 9), [1, 0]);
+        assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), [1, 0]);
     }
 }
 
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    use std::collections::HashMap;
+struct Solution {}
 
-    let mut m: HashMap<i32, i32> = HashMap::new();
-    for (index, num) in nums.iter().enumerate() {
-        match m.get(&(target - *num)) {
-            Some(&index2) => return vec![index as i32, index2],
-            None => m.insert(*num, index as i32),
-        };
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        use std::collections::HashMap;
+
+        let mut m: HashMap<i32, i32> = HashMap::new();
+        for (index, num) in nums.iter().enumerate() {
+            match m.get(&(target - *num)) {
+                Some(&index2) => return vec![index as i32, index2],
+                None => m.insert(*num, index as i32),
+            };
+        }
+
+        vec![]
     }
-
-    vec![]
 }
