5333ab74934bda314df1635050475ac71eada8a3
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-06-19 08:01:32 +0900

    update

--- leetcode/valid-parentheses/rust/src/lib.rs
+++ leetcode/valid-parentheses/rust/src/lib.rs
@@ -4,23 +4,27 @@ mod tests {
 
     #[test]
     fn is_valid_test() {
-        assert_eq!(is_valid(String::from("()")), true);
-        assert_eq!(is_valid(String::from("()[]{}")), true);
-        assert_eq!(is_valid(String::from("(]")), false);
+        assert_eq!(Solution::is_valid(String::from("()")), true);
+        assert_eq!(Solution::is_valid(String::from("()[]{}")), true);
+        assert_eq!(Solution::is_valid(String::from("(]")), false);
     }
 }
 
-pub fn is_valid(s: String) -> bool {
-    let mut stack = Vec::new();
-    for i in s.chars() {
-        match i {
-            '{' => stack.push('}'),
-            '(' => stack.push(')'),
-            '[' => stack.push(']'),
-            '}'|')'|']' if Some(i) != stack.pop() => return false,
-            _ => (),
-        }
-    }
+struct Solution {}
 
-    return stack.is_empty();
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+        for i in s.chars() {
+            match i {
+                '{' => stack.push('}'),
+                '(' => stack.push(')'),
+                '[' => stack.push(']'),
+                '}' | ')' | ']' if Some(i) != stack.pop() => return false,
+                _ => (),
+            }
+        }
+
+        return stack.is_empty();
+    }
 }
