17b6ecf0e144eb1d42e6a1855bcf3d27584a980a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-06-22 08:10:54 +0900

    Add Invert Binary Tree(Rust)

---
+++ leetcode/invert-binary-tree/rust/src/lib.rs
@@ -0,0 +1,37 @@
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+struct Solution {}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+    if let Some(node) = root {
+        let right = node.borrow().right.clone();
+        let left = node.borrow().left.clone();
+        node.borrow_mut().left = Self::invert_tree(right);
+        node.borrow_mut().right = Self::invert_tree(left);
+        return Some(node);
+    } else {
+        return None;
+    }
+}
+}
