eab55786b8ea89f5b2d5dfbd1caf966e322fb748
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-07-05 08:07:57 +0900

    Fix

--- leetcode/add-two-numbers/rust/src/lib.rs
+++ leetcode/add-two-numbers/rust/src/lib.rs
@@ -2,20 +2,23 @@
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
   pub val: i32,
-  pub next: Option<Box<ListNode>>
+  pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
   #[inline]
   fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
-    }
+    ListNode { next: None, val }
   }
 }
 
-pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+struct Solution {}
+
+impl Solution {
+  pub fn add_two_numbers(
+    l1: Option<Box<ListNode>>,
+    l2: Option<Box<ListNode>>,
+  ) -> Option<Box<ListNode>> {
     let mut dummy_head = ListNode::new(0);
     let mut current = &mut dummy_head;
 
@@ -25,24 +28,25 @@ pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> 
     let mut carry: i32 = 0;
 
     while p != None || q != None {
-        let sum = match (&p, &q) {
-            (Some(l1), Some(l2)) => l1.val + l2.val + carry,
-            (Some(l1), None) => l1.val + carry,
-            (None, Some(l2)) => l2.val + carry,
-            (None, None) => carry,
-        };
+      let sum = match (&p, &q) {
+        (Some(l1), Some(l2)) => l1.val + l2.val + carry,
+        (Some(l1), None) => l1.val + carry,
+        (None, Some(l2)) => l2.val + carry,
+        (None, None) => carry,
+      };
 
-        carry = sum / 10;
-        current.next = Some(Box::new(ListNode::new(sum & 10)));
-        current = current.next.as_mut().unwrap();
+      carry = sum / 10;
+      current.next = Some(Box::new(ListNode::new(sum % 10)));
+      current = current.next.as_mut().unwrap();
 
-        p = if p != None { p.unwrap().next } else { p };
-        q = if q != None { q.unwrap().next } else { q };
+      p = if p != None { p.unwrap().next } else { p };
+      q = if q != None { q.unwrap().next } else { q };
     }
 
     if carry > 0 {
-        current.next = Some(Box::new(ListNode::new(carry)));
+      current.next = Some(Box::new(ListNode::new(carry)));
     }
 
     dummy_head.next
+  }
 }
